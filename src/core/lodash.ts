04be5e462f4410d6548c06726eace7255cfce0df
Todor Pavlov <tpavlov@softserveinc.com>
2019-04-10 12:47:18 +0300

    no message

---
+++ src/core/lodash.ts
@@ -0,0 +1,16 @@
+// include only the lodash functions that we use
+export {
+  // collections
+  chunc, concat, drop, difference, fill, findIndex, intersection, join, map, remove, reverse, take, tail, union, uniq, without, each, find, filter,
+  forEach, groupBy, includes, orderBy, reduce, some, sortBy,
+  // functions
+  debounce, delay, once, wrap,
+  // lang
+  isEmpty, cloneDeep, isArray,
+  // object
+  assign, forOwn, get, has, keys, merge, omit, set, values,
+  // strings
+  camelCase, lowerCase, capitalize, escape, kebabCase, replace, trim,
+  // util
+  attempt, range, uniqueId,
+} from 'lodash';
