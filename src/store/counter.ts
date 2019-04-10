28b8f45cad66f7866575bcb9d90bc886ca1df600
Todor Pavlov <tpavlov@softserveinc.com>
2019-04-10 18:08:36 +0300

    no message

---
+++ src/store/counter.ts
@@ -0,0 +1,31 @@
+import { VuexModule, Module, MutationAction, getModule, Mutation, Action } from "vuex-module-decorators";
+import store from "./index";
+
+@Module({ dynamic: true, name: "counter", store: store })
+class Counter extends VuexModule {
+  count = 0;
+  zz = "zz";
+
+  // @MutationAction({ mutate: ["count"] })
+  @Mutation
+  increment() {
+    // return { count: this.count + 1 };
+    this.count++;
+  }
+
+  @Action({ commit: "increment" })
+  inc() {
+    console.log(this.count);
+
+    return 5;
+  }
+
+  @MutationAction({ mutate: ["count"] })
+  async in() {
+    // console.log(this.state.count, this.state.zz);
+    return { count: (this.state as any).count + 1 };
+  }
+}
+console.log(getModule(Counter));
+
+export default getModule(Counter);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    Conter: {{counter.count}} {{counter.zz}}
+    <br>
+    <button @click="counter.inc()">Inc</button>
+    <button @click="counter.in()">zz</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import { getModule } from "vuex-module-decorators";
+import Counter from "@/store/counter";
 
 console.log(_.map([1, 2, 3], v => v * 2));
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+@Component({})
+export default class Home extends Vue {
+  counter = Counter;
+}
 </script>
